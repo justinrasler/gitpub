99301be43dd590f4d6f1a78d3d128ffb4e81d7e5
justinrasler <85715506+justinrasler@users.noreply.github.com>
2021-09-14 21:28:00 -0400

    drink routes created

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000
+const drinks = require("./models/drinks")
 
 app.get('/', (req,res) => {
     res.send("welcome to the GitPub app!")
+})
+app.get('/drinks', (req,res) => {
+    res.send(drinks)
 })
 
 app.listen(port,() => {
