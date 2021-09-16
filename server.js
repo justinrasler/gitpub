3e721952caea60ecbf97e517f07532af8b21a7e3
justinrasler <85715506+justinrasler@users.noreply.github.com>
2021-09-16 13:39:32 -0400

    render individual drinks

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const drinks = require("./models/drinks")
 app.get('/', (req,res) => {
     res.send("welcome to the GitPub app!")
 })
-app.get('/drinks', (req,res) => {
+app.get('/drinks/', (req,res) => {
     res.render('drinks_index.ejs', {allDrinks: drinks})
 })
+
+// app.get('/drinks/x', (req,res) => {
+//     res.render('drinks_show.ejs', {drink: drinks[req.params.indexOfDrinksArray]})
+// })
+
 app.get('/drinks/:id', (req,res) => {
-    res.send(req.params.id)
+    res.render('drinks_show.ejs', {drink: drinks[req.params.id]})
 });
 
 app.listen(port,() => {
