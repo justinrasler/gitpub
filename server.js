a7936af3fa9f90947e9f773717158bacfb6bbd21
justinrasler <85715506+justinrasler@users.noreply.github.com>
2021-09-17 10:24:12 -0400

    added food

--- server.js
+++ server.js
@@ -2,20 +2,31 @@ const express = require("express");
 const app = express();
 const port = 3000
 const drinks = require("./models/drinks")
+const food = require("./models/food")
 
 app.get('/', (req,res) => {
     res.send("welcome to the GitPub app!")
 })
 app.get('/drinks/', (req,res) => {
-    res.render('drinks_index.ejs', {allDrinks: drinks})
+    res.render('drinks_index.ejs', {allDrinks: drinks},)
 })
 
-// app.get('/drinks/x', (req,res) => {
-//     res.render('drinks_show.ejs', {drink: drinks[req.params.indexOfDrinksArray]})
-// })
 
 app.get('/drinks/:id', (req,res) => {
     res.render('drinks_show.ejs', {drink: drinks[req.params.id]})
+});
+
+/////////////////////////
+//food
+///////////////////////////////
+
+app.get('/food/', (req,res) => {
+    res.render('food_index.ejs', {allFood: food})
+})
+
+
+app.get('/food/:id', (req,res) => {
+    res.render('food_show.ejs', {foods: food[req.params.id]})
 });
 
 app.listen(port,() => {
